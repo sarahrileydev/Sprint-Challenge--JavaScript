cdd4a364acd7f45de32514730caa25abc28fadbb
sriley <sarahriley.dev@gmail.com>
2019-03-01 12:47:32 -0500

    object arrays

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -74,7 +74,10 @@ const graduates = [{ "id": 1, "first_name": "Cynde", "university": "Missouri Sou
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+for (let i = 0; i < graduates.length; i++){
+  universities.push(graduates[i].university);
+}
+console.log(universities.sort());
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -83,11 +86,19 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+for (let i = 0; i < graduates.length; i++){
+  contactInfo.push(graduates[i].first_name + " " + graduates[i].email);
+}
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+for (let i = 0; i < graduates.length; i++){
+  if(graduates[i].university.includes("Uni")){
+    uni.push(graduates[i].university);
+  }
+}
 console.log(uni);
 
 
@@ -113,6 +124,9 @@ The zoo wants to display both the scientific name and the animal name in front o
 
 */
 const animalNames = [];
+zooAnimals.forEach(function(currentValue) {
+  return animalNames.push(`Name: ${currentValue.animal_name} Scientific Name: ${currentValue.scientific_name}`)
+});
 console.log(animalNames);
 
 /* Request 2: .map()    
@@ -121,15 +135,21 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 
 */
 
-const lowerCase = [];
+const lowerCase = zooAnimals.map(function(currentValue){
+  return currentValue.animal_name.toLowerCase(); 
+});
 console.log(lowerCase);
+
+
 
 /* Request 3: .filter() 
 
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const largerPopulation = [];
+const largerPopulation = zooAnimals.filter(function(currentValue){
+    return currentValue.population < 5;
+});
 console.log(largerPopulation);
 
 /* Request 4: .reduce() 
